e6f3f530ddaf2527ed2a00cd52abd1a173c31078
Mustafa Saifee <mustafa.saifee@aiven.io>
2022-06-30 18:14:11 +0300

    Add api for text analysis

--- src/Priorities/PrioritiesViz.tsx
+++ src/Priorities/PrioritiesViz.tsx
@@ -1,42 +1,58 @@
-import { PrioritiesDataType } from '../Types';
+import maxBy from 'lodash.maxby';
+import sortBy from 'lodash.sortby';
+import { useState } from 'react';
+import SDGGoalList from '../Data/SDGGoalList.json';
 
-const SDGPriorities: PrioritiesDataType[] = require('../Data/SDGPrioritiesIraq.json');
+interface Props {
+  data: any;
+}
 
-export const PrioritiesViz = () => {
+export const PrioritiesViz = (props: Props) => {
+  const { data } = props;
+  const dataFormatted = sortBy(Object.keys(data), (d) => parseInt(d.split(' ')[1], 10)).map((d) => ({
+    priorityType: d,
+    priorities: data[d],
+  }));
+  const [selectedGoal, setSelectedGoal] = useState<string>(dataFormatted[0].priorityType);
   const rowHeight = 25;
   const headerRowHeight = 60;
-  let noOfPriorities = 0;
-  SDGPriorities.forEach((d) => {
-    noOfPriorities += d.priorities.length;
-  });
 
   const Blue = '#006EB5';
 
   const x1 = 100;
   const x2 = 30;
-  const x3 = 175 + (SDGPriorities.length * 30);
-  const x3Gap = 30;
+  const x3 = 175 + (dataFormatted.length * 30);
+  const x3Gap = 0;
   const x4 = 30;
-  const paddingBetweenCategories = 20;
 
   const cornerRadius = 20;
   const cornerRadiusSmall = 10;
 
+  const radiusFactor = 25 / (maxBy(dataFormatted, (d) => d.priorities.length)?.priorities.length as number);
+
   const colorArray = [
-    '#AF5F68',
-    '#00859B',
-    '#9368A1',
-    '#6A7E3F',
-    '#D12800',
+    '#006EB5',
+    '#e5243b',
+    '#DDA63A',
+    '#4C9F38',
+    '#C5192D',
+    '#FF3A21',
+    '#26BDE2',
+    '#FCC30B',
+    '#A21942',
+    '#FD6925',
+    '#DD1367',
+    '#FD9D24',
+    '#BF8B2E',
+    '#3F7E44',
+    '#0A97D9',
+    '#56C02B',
+    '#00689D',
+    '#19486A',
   ];
 
-  let prevHeight = headerRowHeight;
-  const totalPrevPriorities = [0];
+  const svgHeight = (((dataFormatted[dataFormatted.findIndex((d) => d.priorityType === selectedGoal)].priorities.length) * rowHeight) - cornerRadiusSmall * 2) + (dataFormatted.findIndex((d) => d.priorityType === selectedGoal)) * headerRowHeight + headerRowHeight + 70 > (dataFormatted.length) * headerRowHeight + headerRowHeight + 70 ? (((dataFormatted[dataFormatted.findIndex((d) => d.priorityType === selectedGoal)].priorities.length) * rowHeight) - cornerRadiusSmall * 2) + (dataFormatted.findIndex((d) => d.priorityType === selectedGoal)) * headerRowHeight + headerRowHeight + 70 : (dataFormatted.length) * headerRowHeight + headerRowHeight + 70;
 
-  for (let i = 0; i < SDGPriorities.length - 1; i += 1) {
-    totalPrevPriorities.push(totalPrevPriorities[i] + SDGPriorities[i].priorities.length);
-  }
-  const svgHeight = ((noOfPriorities + 1) * rowHeight) + (paddingBetweenCategories * (SDGPriorities.length - 1)) + headerRowHeight + 20;
   return (
     <svg width='100%' viewBox={`0 0 1280 ${svgHeight}`}>
       <g transform='translate(10,30)'>
@@ -60,7 +76,7 @@ export const PrioritiesViz = () => {
           strokeWidth={3}
         />
         {
-          SDGPriorities.map((_d, i) => (
+          dataFormatted.map((_d, i) => (
             <path
               key={i}
               d={`M 5,0 h ${x1} a${cornerRadius},${cornerRadius} 0 0 1 ${cornerRadius},${cornerRadius} v ${(i + 1) * headerRowHeight - cornerRadius * 2} a${cornerRadius},${cornerRadius} 1 0 0 ${cornerRadius},${cornerRadius} h ${x2}`}
@@ -72,101 +88,117 @@ export const PrioritiesViz = () => {
           ))
         }
         {
-            SDGPriorities.map((d, i) => {
-              if (i > 0) { prevHeight = totalPrevPriorities[i] * rowHeight; }
-              return (
-                <g transform={`translate(${x1 + x2 + cornerRadius * 2 + 5},${(i + 1) * headerRowHeight})`} key={i}>
-                  <text
-                    x={0}
-                    y={0}
-                    dx={10}
-                    dy={-5}
-                    fontSize={16}
-                    fill={colorArray[i % colorArray.length]}
-                  >
-                    {d.priorityType}
-                    {' '}
-                    (
-                    {d.priorities.length}
-                    )
-                  </text>
-                  <circle
-                    cx={0}
-                    cy={0}
-                    r={5}
-                    fill='#ffffff'
-                    stroke={colorArray[i % colorArray.length]}
-                    strokeWidth={3}
-                  />
-                  {
-                  d.priorities.map((p, k) => {
-                    if (i === 0) {
-                      return (
-                        <g key={k}>
-                          <path
-                            d={`M 5,0 h ${x3 - (i * x3Gap)} a${cornerRadiusSmall},${cornerRadiusSmall} 0 0 1 ${cornerRadiusSmall},${cornerRadiusSmall} v ${((k + 1) * rowHeight) - cornerRadiusSmall * 2} a${cornerRadiusSmall},${cornerRadiusSmall} 1 0 0 ${cornerRadiusSmall},${cornerRadiusSmall} h ${x4 + (i * x3Gap)}`}
-                            fill='none'
-                            stroke={colorArray[i % colorArray.length]}
-                            strokeWidth={2}
-                            shapeRendering='crispEdge'
-                          />
-                          <circle
-                            cx={x3 + x4 + cornerRadius + 5}
-                            cy={((k + 1) * rowHeight)}
-                            r={3}
-                            fill='#ffffff'
-                            stroke={colorArray[i % colorArray.length]}
-                            strokeWidth={2}
-                          />
-                          <text
-                            x={x3 + x4 + cornerRadius + 5}
-                            y={((k + 1) * rowHeight)}
-                            fill={colorArray[i % colorArray.length]}
-                            fontSize={14}
-                            dx={8}
-                            dy={4}
-                          >
-                            {p}
-                          </text>
-                        </g>
-                      );
-                    }
-
-                    return (
-                      <g key={k}>
-                        <path
-                          d={`M 5,0 h ${x3 - (i * x3Gap)} a${cornerRadiusSmall},${cornerRadiusSmall} 0 0 1 ${cornerRadiusSmall},${cornerRadiusSmall} v ${((k + 1) * rowHeight) - (cornerRadiusSmall * 2) + prevHeight - (i * headerRowHeight) + (i * paddingBetweenCategories)} a${cornerRadiusSmall},${cornerRadiusSmall} 1 0 0 ${cornerRadiusSmall},${cornerRadiusSmall} h ${x4 + (i * x3Gap)}`}
-                          fill='none'
-                          stroke={colorArray[i % colorArray.length]}
-                          strokeWidth={2}
-                          shapeRendering='crispEdge'
-                        />
-                        <circle
-                          cx={x3 + x4 + cornerRadius + 5}
-                          cy={((k + 1) * rowHeight) + prevHeight - (i * headerRowHeight) + (i * 20)}
-                          r={3}
-                          fill='#ffffff'
-                          stroke={colorArray[i % colorArray.length]}
-                          strokeWidth={2}
-                        />
-                        <text
-                          x={x3 + x4 + cornerRadius + 5}
-                          y={((k + 1) * rowHeight) + prevHeight - (i * headerRowHeight) + (i * 20)}
-                          fill={colorArray[i % colorArray.length]}
-                          fontSize={14}
-                          dx={8}
-                          dy={4}
-                        >
-                          {p}
-                        </text>
-                      </g>
-                    );
-                  })
-                }
-                </g>
-              );
-            })
-            }
+          dataFormatted.map((d, i) => (
+            <g transform={`translate(${x1 + x2 + cornerRadius * 2 + 5},${(i + 1) * headerRowHeight})`} key={i}>
+              {
+                selectedGoal === d.priorityType
+                  ? (
+                    <>
+                      {
+                        d.priorities.map((p: any, k: number) => {
+                          if (i === 0) {
+                            return (
+                              <g key={k}>
+                                <path
+                                  d={`M 5,0 h ${x3 - (i * x3Gap)} a${cornerRadiusSmall},${cornerRadiusSmall} 0 0 1 ${cornerRadiusSmall},${cornerRadiusSmall} v ${((k + 1) * rowHeight) - cornerRadiusSmall * 2} a${cornerRadiusSmall},${cornerRadiusSmall} 1 0 0 ${cornerRadiusSmall},${cornerRadiusSmall} h ${x4 + (i * x3Gap)}`}
+                                  fill='none'
+                                  stroke={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                                  strokeWidth={2}
+                                  shapeRendering='crispEdge'
+                                />
+                                <circle
+                                  cx={x3 + x4 + cornerRadius + 5}
+                                  cy={((k + 1) * rowHeight)}
+                                  r={3}
+                                  fill='#ffffff'
+                                  stroke={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                                  strokeWidth={2}
+                                />
+                                <text
+                                  x={x3 + x4 + cornerRadius + 5}
+                                  y={((k + 1) * rowHeight)}
+                                  fill={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                                  fontSize={14}
+                                  dx={8}
+                                  dy={4}
+                                >
+                                  {p}
+                                </text>
+                              </g>
+                            );
+                          }
+                          return (
+                            <g key={k}>
+                              <path
+                                d={`M 5,0 h ${x3 - (i * x3Gap)} a${cornerRadiusSmall},${cornerRadiusSmall} 0 0 1 ${cornerRadiusSmall},${cornerRadiusSmall} v ${(((k + 1) * rowHeight)) - (cornerRadiusSmall * 2)} a${cornerRadiusSmall},${cornerRadiusSmall} 1 0 0 ${cornerRadiusSmall},${cornerRadiusSmall} h ${x4 + (i * x3Gap)}`}
+                                fill='none'
+                                stroke={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                                strokeWidth={2}
+                                shapeRendering='crispEdge'
+                              />
+                              <circle
+                                cx={x3 + x4 + cornerRadius + 5}
+                                cy={(((k + 1) * rowHeight))}
+                                r={3}
+                                fill='#ffffff'
+                                stroke={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                                strokeWidth={2}
+                              />
+                              <text
+                                x={x3 + x4 + cornerRadius + 5}
+                                y={(((k + 1) * rowHeight))}
+                                fill={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                                fontSize={14}
+                                dx={8}
+                                dy={4}
+                              >
+                                {p}
+                              </text>
+                            </g>
+                          );
+                        })
+                      }
+                    </>
+                  ) : null
+              }
+              <g style={{ cursor: 'pointer' }} onClick={() => { setSelectedGoal(d.priorityType); }}>
+                <text
+                  x={0}
+                  y={0}
+                  dx={5 + radiusFactor * d.priorities.length}
+                  dy={-7}
+                  fontSize={14}
+                  fontWeight='bold'
+                  fill={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                >
+                  {d.priorityType}
+                  {' '}
+                  (
+                  {d.priorities.length}
+                  )
+                </text>
+                <text
+                  x={0}
+                  y={0}
+                  dx={5 + radiusFactor * d.priorities.length}
+                  dy={17}
+                  fontSize={16}
+                  fill={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                >
+                  {SDGGoalList[SDGGoalList.findIndex((goal) => goal.Goal === d.priorityType.replace('Goal', 'SDG'))]['Goal Name']}
+                </text>
+                <circle
+                  cx={0}
+                  cy={0}
+                  r={radiusFactor * d.priorities.length}
+                  fill='#ffffff'
+                  stroke={colorArray[parseInt(d.priorityType.split(' ')[1], 10)]}
+                  strokeWidth={3}
+                />
+              </g>
+            </g>
+          ))
+        }
       </g>
     </svg>
   );
--- src/Priorities/index.tsx
+++ src/Priorities/index.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import styled from 'styled-components';
+import axios from 'axios';
+import { Spin } from 'antd';
+import { useEffect, useState } from 'react';
 import { PrioritiesViz } from './PrioritiesViz';
 import { PageTitle } from '../Components/PageTitle';
 import { Nav } from '../Header/Nav';
@@ -10,12 +14,13 @@ interface Props {
 const RootEl = styled.div`
   width: 128rem;
   margin: 2rem auto 5rem auto;
+  padding-bottom: 5rem;
 `;
 
 const DescriptionEl = styled.div`
-  padding: 2rem;
+  padding: 2rem 2rem 0 2rem;
   background-color: var(--black-200);
-  margin: 0 0 4rem 0;
+  margin: 0 0 2rem 0;
 `;
 
 const FileAttachementEl = styled.div`
@@ -26,7 +31,7 @@ const FileAttachementEl = styled.div`
   font-style: italic;
 `;
 
-const FileAttacehmentButton = styled.div`
+const FileAttacehmentLabel = styled.label`
   background-color: var(--white);
   color: var(--black-700);
   border-radius: 3px;
@@ -36,14 +41,64 @@ const FileAttacehmentButton = styled.div`
   margin-top: 1rem;
   padding: 1rem;
   border: 1px dashed var(--black-550); 
+  width: 100%;
+  justify-content: center;
+  display: flex;
+  cursor: pointer;
+`;
+
+const FileAttacehmentButton = styled.input`
+  display: none;
 `;
 
 const HR = styled.hr`
   margin: 2rem 0 0 0;
 `;
 
+interface FileSelectedBannerElProps {
+  backgroundColor: string;
+  borderColor: string;
+}
+
+const FileSelectedBannerEl = styled.div<FileSelectedBannerElProps>`
+  width: 100%;
+  font-size: 1.6rem;
+  padding: 2rem;
+  margin: 0 0 2rem 0;
+  background-color: ${(props) => props.backgroundColor};
+  color: var(--black-700);
+  border: ${(props) => `1px solid  ${props.borderColor}`};
+`;
+
 export const Priorities = (props: Props) => {
   const { country } = props;
+  const [selectedFile, setSelectedFile] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
+  const [data, setData] = useState<any>(null);
+  useEffect(() => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+      axios({
+        method: 'post',
+        url: 'https://undp.livedata.link/nlp/upload',
+        data: formData,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+        .then((response: any) => {
+          setData(response.data);
+        })
+        .catch((err) => {
+          setError(err.message);
+        });
+    }
+  }, [selectedFile]);
+
+  const handleFileSelect = (event: any) => {
+    setData(null);
+    setError(null);
+    setSelectedFile(event.target.files[0]);
+  };
   return (
     <>
       <Nav
@@ -69,13 +124,55 @@ export const Priorities = (props: Props) => {
                 <>
                   Complement the existing database of national planning documents and voluntary national reviews by uploading a relevant national resource such as a policy brief, assessment, development intervention proposal, etc. to analyse and identify Acceleration Opportunities
                 </>
-                <FileAttacehmentButton>
-                  Attach a file
-                </FileAttacehmentButton>
+                <div>
+                  <FileAttacehmentLabel htmlFor='file-upload' className='custom-file-upload'>
+                    Attach a File
+                  </FileAttacehmentLabel>
+                  <FileAttacehmentButton id='file-upload' type='file' onChange={handleFileSelect} />
+                </div>
               </FileAttachementEl>
             </>
           </DescriptionEl>
-          <PrioritiesViz />
+          {
+            selectedFile
+              ? !data
+                ? error ? (
+                  <FileSelectedBannerEl backgroundColor='var(--accent-red-light)' borderColor='var(--accent-red)'>
+                    Error Processing
+                    {' '}
+                    <span className='bold'>{selectedFile.name}</span>
+                    :
+                    {' '}
+                    {error}
+                    {' '}
+                    (please check the file is pdf format and try again)
+                  </FileSelectedBannerEl>
+                )
+                  : (
+                    <FileSelectedBannerEl backgroundColor='var(--accent-yellow-light)' borderColor='var(--accent-yellow)'>
+                      Processing
+                      {' '}
+                      <span className='bold'>{selectedFile.name}</span>
+                    </FileSelectedBannerEl>
+                  ) : (
+                    <FileSelectedBannerEl backgroundColor='var(--accent-green-light)' borderColor='var(--accent-green)'>
+                      Showing results for
+                      {' '}
+                      <span className='bold'>{selectedFile.name}</span>
+                    </FileSelectedBannerEl>
+                )
+              : null
+          }
+          {
+            selectedFile && !error ? (
+              <>
+                {
+                  data ? <PrioritiesViz data={data} /> : <Spin size='large' />
+                }
+              </>
+            )
+              : null
+          }
         </RootEl>
       </div>
     </>
