159de4f0c4f8b9de040ca88c4b095eb5034e0c87
GedionT <teshomegedion@gmail.com>
2024-10-17 16:44:03 +0300

    Refactor(api): changed endpoints to updated api endpoint

--- src/api/prioritiesCall.ts
+++ src/api/prioritiesCall.ts
@@ -14,7 +14,7 @@ export const fetchMetadata = async (
     kind,
   };
   const response = await axios.get(
-    `${AIAAS_API_BASE_URL}/classification/metadata`,
+    `${AIAAS_API_BASE_URL}/diagnostic/metadata`,
     {
       params,
       headers: {
@@ -27,7 +27,7 @@ export const fetchMetadata = async (
 };
 
 export const fetchDocumentById = async (id: string) => {
-  const response = await axios.get(`${AIAAS_API_BASE_URL}/classification/diagnostics/${id}`, {
+  const response = await axios.get(`${AIAAS_API_BASE_URL}/diagnostic/${id}`, {
     headers: {
       accept: 'application/json',
       api_key: AIAAS_API_KEY,
@@ -49,7 +49,7 @@ export const submitDocumentsForAnalysis = async (
     : '?features=true';
 
   const response = await axios.post(
-    `${AIAAS_API_BASE_URL}/classification/diagnostics${weightAndFeaturesParam}`,
+    `${AIAAS_API_BASE_URL}/diagnostic${weightAndFeaturesParam}`,
     formData,
     {
       headers: {
@@ -62,7 +62,7 @@ export const submitDocumentsForAnalysis = async (
   return response.data;
 };
 
-export async function detectLanguages(texts: string[]) {
+export async function detectLanguageViaAPI(texts: string[]) {
   try {
     const response = await axios({
       method: 'post',
@@ -80,7 +80,7 @@ export async function detectLanguages(texts: string[]) {
   }
 }
 
-export async function extractViaAPI(fileData: File) {
+export async function extractTextViaAPI(fileData: File) {
   const formData = new FormData();
   formData.append('file', fileData);
 
